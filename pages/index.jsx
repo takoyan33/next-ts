aeba83b201c3028796d482bf3f681a65c7bff990
abe <harrier2070@gmail.com>
2022-06-02 14:05:23 +0900

    修正

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,66 +1,10 @@
-// import Head from "next/head";
-// import Layout, { siteTitle } from "../components/layout";
-// import utilStyles from "../styles/utils.module.css";
-// import { getSortedPostsData } from "../lib/posts";
-// import Link from "next/link";
-// import Date from "../components/date";
-
-// export default function Home({ allPostsData }) {
-//   return (
-//     <Layout home>
-//       <Head>
-//         <title>{siteTitle}</title>
-//       </Head>
-//       <section className={utilStyles.headingMd}>
-//         <p className="text-red-400">API図鑑</p>
-//         <p>このサイトでは、様々なapiの使い方を学ぶことができます。</p>
-
-//         <Link href="resas">Resas API編</Link>
-//         <br></br>
-//         <Link href="spotify">Spotify API編</Link>
-//         <br></br>
-//         <Link href="resas">Line API編</Link>
-//         <br></br>
-//         <Link href="new">新規登録</Link>
-//         <br></br>
-//         <Link href="login">ログイン</Link>
-//       </section>
-//       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-//         <h2 className={utilStyles.headingLg}>ブログ</h2>
-//         <ul className={utilStyles.list}>
-//           {allPostsData.map(({ id, date, title }) => (
-//             <li className={utilStyles.listItem} key={id}>
-//               <Link href={`/posts/${id}`}>
-//                 <a>{title}</a>
-//               </Link>
-//               <br />
-//               <small className={utilStyles.lightText}>
-//                 <Date dateString={date} />
-//               </small>
-//             </li>
-//           ))}
-//         </ul>
-//       </section>
-//     </Layout>
-//   );
-// }
-
-// export async function getStaticProps() {
-//   const allPostsData = getSortedPostsData();
-//   return {
-//     props: {
-//       allPostsData,
-//     },
-//   };
-// }
-
 import React from "react";
 
 import Layout from "../components/layout";
-import { useUser } from "@auth0/nextjs-auth0";
+import { useFetchUser } from "../lib/user";
 
 export default function Home() {
-  const { user, loading } = useUser();
+  const { user, loading } = useFetchUser();
 
   return (
     <Layout user={user} loading={loading}>
